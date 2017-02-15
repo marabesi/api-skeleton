c01f749f4728fd890539deaec0e6606ab8042fb7
marabesi <matheus.marabesi@gmail.com>
2017-02-15 16:36:38 -0200

    added mongodb support

--- resources/config/database.php
+++ resources/config/database.php
@@ -27,3 +27,19 @@ $app->register(new DoctrineOrmServiceProvider, array(
         ),
     ),
 ));
+
+use Doctrine\MongoDB\Connection;
+use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+AnnotationDriver::registerAnnotationClasses();
+
+$config = new Configuration();
+$config->setProxyDir(__DIR__ . '/../../storage/cache/proxies');
+$config->setProxyNamespace('Proxies');
+$config->setHydratorDir(__DIR__ . '/../../storage/cache/hydrators');
+$config->setHydratorNamespace('Hydrators');
+$config->setMetadataDriverImpl(AnnotationDriver::create(__DIR__ . '/../../src/App/Documents'));
+
+$dm = DocumentManager::create(new Connection(), $config);
