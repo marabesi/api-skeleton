f6a920c1d7c0bf4723a76f82b1dc4d591f6ebd52
Matheus Marabesi <matheus.marabesi@gmail.com>
2017-08-26 22:56:48 -0300

    Update mongo-php-driver-installer.sh
    added -y as an argument

--- mongo-php-driver-installer.sh
+++ mongo-php-driver-installer.sh
@@ -3,7 +3,7 @@
 wget http://pecl.php.net/get/mongo-1.2.10.tgz
 tar -xzf mongo-1.2.10.tgz
 
-sudo apt-add-repository ppa:ondrej/php
+sudo apt-add-repository ppa:ondrej/php -y
 sudo apt-get update
 
 sudo apt-get install php7.0-dev
