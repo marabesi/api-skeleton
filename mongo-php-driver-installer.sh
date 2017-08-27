d77ac7bd4ed84a6cf799a8f3519f98682168e7c3
Matheus Marabesi <matheus.marabesi@gmail.com>
2017-08-26 23:01:44 -0300

    Update mongo-php-driver-installer.sh
    changed mongodb from tar to use apt-get

--- mongo-php-driver-installer.sh
+++ mongo-php-driver-installer.sh
@@ -6,6 +6,9 @@ tar -xzf mongo-1.2.10.tgz
 sudo apt-add-repository ppa:ondrej/php -y
 sudo apt-get update
 
-sudo apt-get install php7.0-dev
-sh -c "cd mongo-1.2.10 && phpize && ./configure && sudo make install"
-echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+sudo apt-get install php7.0-dev -y
+
+sudo apt-get install php-pear -y
+
+sudo apt-get install php-mongodb -y
+sudo pecl install mongodb -y
